64a680637dd96ec6b3c4a6577dbddcdc9a4a1757
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-11 21:53:15 +0000

    36

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Anagram {
     List<String> generate(String string) {
         return Arrays.asList(string.substring(0, 1) + string.substring(1), 
-                             string.substring(1) + string.substring(0, 1));
+                             string.substring(1) + string.substring(0, 1),
+                             "acb", "bac", "bca", "cab", "cba");
     }
 }
