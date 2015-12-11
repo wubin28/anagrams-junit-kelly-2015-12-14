00ba023702c51ee0c7ce9cfa9f2c493d7477ce97
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-11 21:17:00 +0000

    10

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -4,13 +4,6 @@ import static org.junit.Assert.*;
 public class AnagramTest {
 
     @Test
-    public void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = Anagram.answer();
-        assertEquals(expected, actual);
-    }
-
-    @Test
     public void the_anagram_of_a_one_letter_word_should_be_itself() {
         // Given
         Anagram anagram = new Anagram();
