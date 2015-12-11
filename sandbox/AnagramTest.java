1b0c6262c370b0bd3846ca35f5b98f66a802e4d5
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-11 21:34:03 +0000

    22

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -13,14 +13,14 @@ public class AnagramTest {
     }
 
 
-    @Test @Ignore
+    @Test
     public void the_anagram_of_a_two_letter_word_should_be_two_words() {
         // Given
         Anagram anagram = new Anagram();
 
         // When & Then
-        assertEquals(true,anagram.generate("ab").contains("ab"));
-        assertEquals(true,anagram.generate("ab").contains("ba"));
+        assertTrue(anagram.generate("ab").contains("ab"));
+        assertTrue(anagram.generate("ab").contains("ba"));
 
 
     }
