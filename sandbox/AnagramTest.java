e3e4ae75dd9f01d3cf7fc113444aea43dd33533c
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-15 06:08:19 +0000

    76

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -7,16 +7,16 @@ public class AnagramTest {
 
     @Test
     public void the_anagram_of_a_one_letter_word_should_be_itself() {
-        assertThat(Anagram.of("a"), is(Arrays.asList("a")));
+        assertThat(Anagrams.of("a"), is(Arrays.asList("a")));
     }
 
     @Test
     public void the_anagram_of_a_two_letter_word_should_be_two_words() {
-        assertThat(Anagram.of("ab"), is(Arrays.asList("ab", "ba")));
+        assertThat(Anagrams.of("ab"), is(Arrays.asList("ab", "ba")));
     }
 
     @Test
     public void the_anagram_of_a_three_letter_word_should_be_six_words() {
-        assertThat(Anagram.of("abc"), is(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba")));
+        assertThat(Anagrams.of("abc"), is(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba")));
     }
 }
