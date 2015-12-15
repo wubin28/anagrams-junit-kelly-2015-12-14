74a0f19246e3ad5a2d04138b99eec713fd60f771
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-15 05:43:41 +0000

    55

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -7,15 +7,21 @@ public class Anagram {
         if (s.length() == 1) {
             return Arrays.asList(s);
         }
+
         if (s.length() == 2) {
             return Arrays.asList(s.substring(0, 1) + s.substring(1), 
                              s.substring(1, 2) + s.substring(0, 1));
         }
-        return Arrays.asList(s.substring(0, 1) + Anagram.of("bc").get(0), 
-                             s.substring(0, 1) + Anagram.of("bc").get(1), 
-                             s.substring(1, 2) + Anagram.of("ac").get(0), 
-                             s.substring(1, 2) + Anagram.of("ac").get(1), 
-                             s.substring(2, 3) + Anagram.of("ab").get(0), 
-                             s.substring(2, 3) + Anagram.of("ab").get(1));
+
+        List<String> anagrams = new ArrayList<String>();
+
+        anagrams.add(s.substring(0, 1) + Anagram.of("bc").get(0));
+        anagrams.add(s.substring(0, 1) + Anagram.of("bc").get(1));
+        anagrams.add(s.substring(1, 2) + Anagram.of("ac").get(0));
+        anagrams.add(s.substring(1, 2) + Anagram.of("ac").get(1));
+        anagrams.add(s.substring(2, 3) + Anagram.of("ab").get(0));
+        anagrams.add(s.substring(2, 3) + Anagram.of("ab").get(1));
+
+        return anagrams;
     }
 }
