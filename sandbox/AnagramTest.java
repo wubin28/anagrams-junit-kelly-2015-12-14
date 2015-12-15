7cedc9da0fd760b6b9d3dc6861213aaace126fb9
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-15 05:29:38 +0000

    41

--- sandbox/AnagramTest.java
+++ sandbox/AnagramTest.java
@@ -6,40 +6,16 @@ public class AnagramTest {
 
     @Test
     public void the_anagram_of_a_one_letter_word_should_be_itself() {
-        // Given
-        Anagram anagram = new Anagram();
-
-        // When & Then
-        assertTrue(anagram.generate("a").contains("a"));
+        assertThat(Anagram.of("a"), is(Arrays.asList("a")));
     }
 
     @Test
     public void the_anagram_of_a_two_letter_word_should_be_two_words() {
-        // Given
-        Anagram anagram = new Anagram();
-
-        // When
-        List<String> result = anagram.generate("ab");
-
-        //Then
-        assertTrue(result.contains("ab"));
-        assertTrue(result.contains("ba"));
+        assertThat(Anagram.of("ab"), is(Arrays.asList("ab", "ba"));
     }
 
     @Test
     public void the_anagram_of_a_three_letter_word_should_be_six_words() {
-        // Given
-        Anagram anagram = new Anagram();
-
-        // When
-        List<String> result = anagram.generate("abc");
-
-        //Then
-        assertTrue(result.contains("abc"));
-        assertTrue(result.contains("acb"));
-        assertTrue(result.contains("bac"));
-        assertTrue(result.contains("bca"));
-        assertTrue(result.contains("cab"));
-        assertTrue(result.contains("cba"));
+        assertThat(Anagram.of("abc"), is(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba"));
     }
 }
