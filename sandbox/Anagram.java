942f3d84fd085a2e24b5b33bcff2cedc4ad3f271
lion_BED6A67BBF <lion@cyber-dojo.org>
2015-12-15 05:34:06 +0000

    48

--- sandbox/Anagram.java
+++ sandbox/Anagram.java
@@ -3,10 +3,13 @@ import java.util.List;
 import java.util.Arrays;
 
 public class Anagram {
-    public static List<String> of(String string) {
-        if (string.length() == 2) {
-        return Arrays.asList(string.substring(0, 1) + string.substring(1), 
-                             string.substring(1) + string.substring(0, 1));
+    public static List<String> of(String s) {
+        if (s.length() == 1) {
+            return Arrays.asList(s);
+        }
+        if (s.length() == 2) {
+            return Arrays.asList(s.substring(0, 1) + s.substring(1), 
+                             s.substring(1) + s.substring(0, 1));
         }
         return Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba");
     }
